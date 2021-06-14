ea5a846311f1520cb3399cd8aecdcf7c0d9c0305
hibiki0827 <73951037+hibiki0827@users.noreply.github.com>
2021-06-14 09:58:25 +0200

    Add files via upload

---
+++ project/backsta.py
@@ -0,0 +1,38 @@
+
+#%%
+print("import...", "\n")
+import os
+import numpy as np
+import mne
+print("done", "\n")
+
+#%%
+
+#read data
+print("read data...","\n")
+
+data_path = "D:/Documents/Uni/Master/2.FS/project_work/data/ds003061-download"
+misc = ("EXG1", "EXG2", "EXG3", "EXG4", "EXG5", "EXG6", "EXG7", "EXG8")
+exclude = ()
+names = {}
+
+for i in range(13):
+    print(i)
+    subs = ("001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011","012", "013")
+    subnum = subs[i]
+    for k in range(3):
+        blocks = ("01", "02", "03")
+        block = blocks[k]
+        if block == "03" and subnum == "006":
+            names[f"s_{subnum}b_{block}"] = "doesn't exist"
+        elif block == "01" and subnum == "007":
+            names[f"s_{subnum}b_{block}"] = "doesn't exist"
+        else:
+            filename = f"sub-{subnum}_task-P300_run-{block}_eeg.bdf"
+            names[f"s_{subnum}b_{block}"] =  mne.io.read_raw_bdf(f"{data_path}/sub-{subnum}/eeg/{filename}", eog=None, misc=misc, stim_channel='auto', exclude=exclude, preload=False, verbose=None)
+
+print("\n","done","\n")
+
+
+
+# %%
